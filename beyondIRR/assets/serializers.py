4e24b3defa51441ce1f311f8b22eac3c9f713946
Kaustubh Verma <vermakaustubh28@gmail.com>
2024-08-17 00:18:41 +0530

    created models and serializer for user model

---
+++ beyondIRR/assets/serializers.py
@@ -0,0 +1,20 @@
+from .models import User
+from rest_framework import serializers
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['email', 'password', 'first_name', 'last_name', 'arn_number']
+
+    def create(self, validated_data):
+        user = User(
+            email=validated_data['email'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
+            arn_number=validated_data['arn_number'],
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
